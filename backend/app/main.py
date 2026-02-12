133c6e9e4d2e982c8f4cfa7d3e906c22b1e7b743
yuntyu01 <yuntyu01@gmail.com>
2026-02-12 21:38:07 +0900

    폴더 구조 수정

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+# poetry shell
+# poetry run uvicorn main:app --reload
+from fastapi import FastAPI
+from typing import Union  
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Dailo World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
